6546a75acceee350ee96f3fb23444f05f5322d1e
Garry Zverev <garry1287@gmail.com>
2021-05-23 12:28:39 +0300

    Add urlmarkdown-randomdate

--- urlmakrdown-randomdate.py
+++ urlmakrdown-randomdate.py
@@ -2,6 +2,20 @@ import re
 import os
 import datetime
 import pytz
+from random import randrange
+from datetime import timedelta
+
+# Для старых файлов рандомно выставляем время
+
+def random_date(start, end):
+    """
+    This function will return a random datetime between two datetime 
+    objects.
+    """
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = randrange(int_delta)
+    return start + timedelta(seconds=random_second)
 
 # https://coderoad.ru/47204017/%D0%9E%D1%82%D0%BA%D1%80%D1%8B%D1%82%D0%B8%D0%B5-%D0%B8-%D1%87%D1%82%D0%B5%D0%BD%D0%B8%D0%B5-%D1%84%D0%B0%D0%B9%D0%BB%D0%BE%D0%B2-%D0%B2-%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3%D0%B5-%D0%B2-python-python-%D0%BD%D0%B0%D1%87%D0%B8%D0%BD%D0%B0%D1%8E%D1%89%D0%B8%D1%85
 # https://coderoad.ru/40279488/%D0%9A%D0%B0%D0%BA-%D0%BF%D1%80%D0%BE%D1%87%D0%B8%D1%82%D0%B0%D1%82%D1%8C-%D0%BC%D0%BD%D0%BE%D0%B3%D0%BE-txt-%D1%84%D0%B0%D0%B9%D0%BB%D0%BE%D0%B2-%D0%B2-%D0%BE%D0%BF%D1%80%D0%B5%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D0%BE%D0%B9-%D0%BF%D0%B0%D0%BF%D0%BA%D0%B5-%D1%81-%D0%BF%D0%BE%D0%BC%D0%BE%D1%89%D1%8C%D1%8E-python
@@ -20,8 +34,18 @@ import pytz
 path = "/home/garry/from-notebook/it-garry/MyBlog/iptech-blog/test/"
 pattern = re.compile(r'(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))')
 
+# Пришёл в PSI
+d1 = datetime.datetime.strptime("1.12.2008 00:00", "%d.%m.%Y %H:%M")
+# Делал эти заметки до этого времени
+d2 = datetime.datetime.strptime('31.12.2018 23:59', "%d.%m.%Y %H:%M")
 
-def create_file_header(FileName):
+tz_moscow = pytz.timezone('Europe/Moscow')
+
+
+
+#(random_date(d1, d2))
+
+def create_file_header(FileName, MyRandomDate):
     return """---
 layout: post
 title:  "%s"
@@ -31,7 +55,10 @@ categories: Git and docs
 
 # %s
 
-""" % (FileName, datetime.datetime.now(pytz.timezone('Europe/Moscow')).strftime("%Y-%m-%d %H:%M:%S %z"), FileName)
+""" % (FileName, MyRandomDate.strftime("%Y-%m-%d %H:%M:%S %z"), FileName)
+
+
+
 
 
 # returns the names of the files in the directory data as a list
@@ -40,9 +67,10 @@ lines = []
 for file in list_of_files:
     # Преобразование имени файла в markdown
     f = open(os.path.join(path + file), "r")
-    newfile = datetime.datetime.now(pytz.timezone('Europe/Moscow')).strftime("%Y-%m-%d") + "-" + file.rpartition('.')[0] + ".markdown"
+    MyRandomDate = tz_moscow.localize(random_date(d1, d2))
+    newfile = MyRandomDate.strftime("%Y-%m-%d") + "-" + file.rpartition('.')[0] + ".markdown"
     fnew = open(os.path.join(path + newfile), "w")
-    fnew.write(create_file_header(newfile))
+    fnew.write(create_file_header(newfile, MyRandomDate))
     # append each line in the file to a list
     lines = f.readlines()
     for line in lines:
